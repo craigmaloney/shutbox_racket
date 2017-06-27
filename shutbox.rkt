af1f9973a07b022f955092cb3c5822e8d59b70c4
Craig Maloney <craig@decafbad.net>
2017-06-27 14:26:04 -0400

    Starting end-of-game routine

--- shutbox.rkt
+++ shutbox.rkt
@@ -32,22 +32,22 @@
   (if (not took-turn) 
     (if (list-check tilelist tiles)
       (for ([i tilelist])
-          (if (index-of tiles i)
-              (begin
+           (if (index-of tiles i)
+             (begin
                (set! tiles (remove i tiles))
                (set! took-turn #t))
-              (error "Tile already shut")))
-    (println "Tile not available to be shut"))
-  (println "Already took your turn")))
+             (error "Tile already shut")))
+      (println "Tile not available to be shut"))
+    (println "Already took your turn")))
 
 (define (check-roll dice-sum tile-sum)
   (= dice-sum tile-sum))
 
 (define (player-turn tilelist)
   (if (check-roll (sum-of-dice) (sum-of-tiles tilelist))
-      (shut-tiles tilelist)
-      (println "Roll does not equal shut tiles. Try again.")))
-  
+    (shut-tiles tilelist)
+    (println "Roll does not equal shut tiles. Try again.")))
+
 (define (my-read-line)
   (let ([contents (read-line)])
     (if (string=? contents "")
@@ -69,13 +69,19 @@
   (println tiles)
   (printf "Dice roll ~v ~v = ~v\n" die1 die2 (sum-of-dice) ))
 
+(define (end-of-game-test sum)
+  (let ((orig-sum sum))
+  (cond 
+    [(zero? sum) #t])
+  (set! sum (max sum 9))
+    ))
+
+#| Main Loop |#
 (start-game)
 (dice-roll)
 (let loop()
-  
   (show-turn)
   (define tilelist (input))
   (player-turn tilelist)
   (next-turn)
-  (when (not end-of-game) (loop))
-  )
+  (when (not end-of-game) (loop)))
